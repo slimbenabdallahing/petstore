cf8d5ccec7888c828d25485d3f42b75aa798f405
slimbenabdallahing <salim.benabdallah.ing@gmail.com>
2023-06-18 19:17:14 +0100

    commit 1

---
+++ src/test/java/com/dhmit/PetstoreTest.java
@@ -0,0 +1,15 @@
+package com.dhmit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PetstoreTest {
+    @Test
+    public void testAddition() {
+        int a = 2;
+        int b = 3;
+        int result = a + b;
+        assertEquals(5, result);
+    }
+}
